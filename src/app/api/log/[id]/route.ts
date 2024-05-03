0c95fcef422ff8cddf1f21fba6ceae9bcc76cdf6
GreatUsername63 <RamirezSantiago244@gmail.com>
2024-05-03 13:14:32 -0700

    controlers logs

--- src/app/api/log/[id]/route.ts
+++ src/app/api/log/[id]/route.ts
@@ -1,20 +1,91 @@
-
 import { NextResponse } from "next/server";
+import { prisma } from "@/libs/prisma";
 
-export function GET() {
-    return NextResponse.json({
-        message: 'Get single log'
-    })
+interface Params {
+    params: { id: string }
 }
 
-export function DELETE() {
-    return NextResponse.json({
-        message: 'Delete single log'
-    })
+export async function GET(request: Request, { params }: Params) {
+    try {
+        const log = await prisma.log.findFirst({
+            where: {
+                id: Number(params.id),
+            },
+        });
+
+        if (!log)
+            return NextResponse.json({message: "Log not found" }, {status: 404});
+
+        return NextResponse.json(log)
+    } catch (error) {
+        if (error instanceof Error) {
+            return NextResponse.json(
+                {
+                    message: error.message,
+                },
+                {
+                    status: 500,
+                }
+            );
+        }
+    }
 }
 
-export function PUT() {
-    return NextResponse.json({
-        message: 'Update single log'
-    })
+export async function DELETE(request: Request, { params }: Params) {
+    try {
+        const log = await prisma.log.delete({
+            where: {
+                id: Number(params.id),
+            },
+        });
+
+        if (!log)
+            return NextResponse.json({message: "Log not found" }, {status: 404});
+
+        return NextResponse.json(log)
+    } catch (error) {
+        if (error instanceof Error) {
+            return NextResponse.json(
+                {
+                    message: error.message,
+                },
+                {
+                    status: 500,
+                }
+            );
+        }
+    }
+}
+
+export async function PUT(request: Request, { params }: Params) {
+    try{
+        const { content, dateTime, userId } = await request.json()
+
+        const log = await prisma.log.update({
+            where: {
+                id: Number(params.id),
+            },
+            data: {
+                content,
+                dateTime: new Date(dateTime),
+                userId
+            }
+        });
+
+        if (!log)
+            return NextResponse.json({message: "Log not found" }, {status: 404});
+
+        return NextResponse.json(log)
+    } catch (error) {
+        if (error instanceof Error) {
+            return NextResponse.json(
+                {
+                    message: error.message,
+                },
+                {
+                    status: 500,
+                }
+            );
+        }
+    }
 }
--- src/app/api/log/route.ts
+++ src/app/api/log/route.ts
@@ -1,15 +1,46 @@
 import { NextResponse } from "next/server";
+import { prisma } from "@/libs/prisma";
 
-export function GET() {
-    return NextResponse.json({
-        message: 'Get all logs'
-    })
+export async function GET() {
+    try {
+        const logs = await prisma.log.findMany()
+        return NextResponse.json(logs)
+    } catch (error) {
+        if (error instanceof Error) {
+            return NextResponse.json(
+                {
+                    message: error.message,
+                },
+                {
+                    status: 500,
+                }
+            );
+        }
+    }
 }
 
-//Todo: get logs for specific user
+export async function POST(request: Request) {
+    try {
+        const { content, dateTime, userId } = await request.json()
+        const newLog = await prisma.log.create({
+            data: {
+                content,
+                dateTime: new Date(dateTime),
+                userId
+            }
+        })
 
-export function POST() {
-    return NextResponse.json({
-        message: 'Post logs'
-    })
+        return NextResponse.json(newLog);
+    } catch (error) {
+        if (error instanceof Error) {
+            return NextResponse.json(
+                {
+                    message: error.message,
+                },
+                {
+                    status: 500,
+                }
+            );
+        }
+    }
 }
