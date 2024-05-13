1eaa173631603f5e935f6c078e50e74246843c8f
GreatUsername63 <RamirezSantiago244@gmail.com>
2024-05-12 20:01:18 -0700

    added passwords to user schema

--- prisma/seeding/seed.js
+++ prisma/seeding/seed.js
@@ -3,13 +3,32 @@ import { faker } from "@faker-js/faker"
 
 const prisma = new PrismaClient();
 
+async function clearData() {
+  try {
+    await prisma.$executeRaw`SET foreign_key_checks = 0`;
+
+    await prisma.user.deleteMany()
+    await prisma.task.deleteMany()
+    await prisma.log.deleteMany()
+
+    await prisma.$executeRaw`SET foreign_key_checks = 1`;
+    await prisma.$executeRaw`ALTER TABLE user AUTO_INCREMENT = 1`;
+
+    console.log("All data deleted successfully");
+  } catch (error) {
+    console.log("Error deleting data: ", error);
+  }
+}
+
 async function main() {
+  await clearData();
 
   //Users
   for (let i = 0; i < 3; i++) {
     await prisma.user.create({
       data: {
-        name: faker.person.firstName()
+        name: faker.person.firstName(),
+        password: faker.internet.password()
       }
     })
   }
