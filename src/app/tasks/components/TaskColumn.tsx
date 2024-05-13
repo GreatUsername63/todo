12fe4c0ba8628f3c4c4cb290f01401d5d934f556
GreatUsername63 <RamirezSantiago244@gmail.com>
2024-05-12 17:59:42 -0700

    completed checkbox works

--- src/app/tasks/components/TaskColumn.tsx
+++ src/app/tasks/components/TaskColumn.tsx
@@ -5,6 +5,18 @@ import { MdDelete } from "react-icons/md";
 
 export default function Home(props: any) {
 
+    const toggleCheckbox = async () => {
+        props.data.complete = !props.data.complete
+        const res = await fetch('http://localhost:3000/api/task/' + props.data.id, {
+            method: 'PUT',
+            body: JSON.stringify({ "name": props.data.name, "description": props.data.description, "dueDate": props.data.dueDate, "userId": 1, "complete": props.data.complete }),
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        })
+        const data = await res.json()
+        console.log(data)
+    }
 
     return (
         <tr>
@@ -14,7 +26,7 @@ export default function Home(props: any) {
             <td>{props.data.dueDate}</td>
             <td>
                 <label>
-                    <input type="checkbox" className="checkbox" defaultChecked={props.data.complete} />
+                    <input type="checkbox" className="checkbox" defaultChecked={props.data.complete} onClick={toggleCheckbox} />
                 </label>
             </td>
             <td>
